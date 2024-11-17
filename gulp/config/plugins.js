81368adf4758186b7f53ab144f6ba5e5f870d8e8
Anna Kessler <kessler.anna96@gmail.com>
2024-11-18 02:01:17 +0300

    Добавление лицензии MIT

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -4,7 +4,6 @@ import plumber from "gulp-plumber";
 import notify from "gulp-notify";
 import size from "gulp-size";
 import rename from "gulp-rename";
-import fileInclude from "gulp-file-include";
 import ifPlugin from "gulp-if";
 import newer from "gulp-newer";
 
