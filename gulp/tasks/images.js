177db81e12e9e14592db1a98b206ea2c80049d0e
Анна Кесслер <kessler.anna96@gmail.com>
2025-09-13 20:40:00 +0300

    🐞 Fix: Перенос изображений

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,14 +1,13 @@
+import path from 'path';
 import webp from 'gulp-webp';
 import imagemin from 'gulp-imagemin';
 
-export const images = async () => {
-	await app.gulp
-		.src(`${app.path.src.images}**/*.webp`, { encoding: false })
-		.pipe(app.plugins.newer(app.path.build.images))
-		.pipe(app.gulp.dest(app.path.build.images));
-
-	await app.gulp
-		.src(`${app.path.src.images}**/*.{jpg,jpeg,png}`, { encoding: false })
+export const images = () => {
+	return app.gulp
+		.src(
+			[`${app.path.src.images}**/*.{jpg,jpeg,png,webp,gif,svg}`, `!${app.path.src.images}svg/**/*`],
+			{ encoding: false },
+		)
 		.pipe(
 			app.plugins.plumber({
 				errorHandler: app.plugins.notify.onError({
@@ -18,14 +17,9 @@ export const images = async () => {
 			}),
 		)
 		.pipe(app.plugins.newer(app.path.build.images))
-		.pipe(webp())
-		.pipe(app.gulp.dest(app.path.build.images));
-
-	return app.gulp
-		.src([`${app.path.src.images}**/*.{gif,svg}`, `!${app.path.src.images}svg/**/*`], {
-			encoding: false,
-		})
-		.pipe(app.plugins.newer(app.path.build.images))
+		.pipe(
+			app.plugins.if((file) => ['.jpg', '.jpeg', '.png'].includes(path.extname(file.path)), webp()),
+		)
 		.pipe(
 			app.plugins.if(
 				app.isProd,
