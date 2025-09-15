6274bad37934165d82bdf1521f97ff6bd009a623
Anna Kessler <kessler.anna96@gmail.com>
2025-09-15 20:17:25 +0300

    🐞 Fix: Favicon

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -33,7 +33,7 @@ export const html = () => {
 			}),
 		)
 		.pipe(app.plugins.replace(/@img\//g, 'img/'))
-		.pipe(app.plugins.if(app.isProd, app.plugins.replace(/\.(jpg|jpeg|png)/g, '.webp')))
+		.pipe(app.plugins.if(app.isProd, app.plugins.replace(/(?<!favicon)\.(jpg|jpeg|png)/g, '.webp')))
 		.pipe(
 			app.plugins.if(
 				app.isProd,
